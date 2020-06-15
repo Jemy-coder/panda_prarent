d9e13eb50c504da0f77ad595cce0ad196fbb1da2
Jemy-coder <mjl520PHM>
2020-06-15 13:31:43 +0800

    生成代码类修改

---
+++ service/edu/src/main/java/com/dragon/controller/TeacherController.java
@@ -0,0 +1,37 @@
+package com.dragon.controller;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.dragon.entity.Teacher;
+import com.dragon.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author JEMY
+ * @since 2020-06-15
+ */
+@RestController
+@RequestMapping("/teacher")
+public class TeacherController {
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @GetMapping("list")
+    public List<Teacher> list(){
+        QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
+        return teacherService.list(queryWrapper);
+    }
+
+}
+
---
+++ service/edu/src/main/java/com/dragon/mapper/TeacherMapper.java
@@ -0,0 +1,16 @@
+package com.dragon.mapper;
+
+import com.dragon.entity.Teacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author JEMY
+ * @since 2020-06-15
+ */
+public interface TeacherMapper extends BaseMapper<Teacher> {
+
+}
